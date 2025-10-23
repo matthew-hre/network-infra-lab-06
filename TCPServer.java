6bc90ee5c8afce1d6069db96cc077a1e39591184
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-10-23 10:28:50 -0600

    feat: initial commit

---
+++ TCPServer.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	public static void main(String argv[]) throws Exception {
+		String clientSentence;
+		String capitalizedSentence;
+
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+
+		while (true) {
+
+			System.out.println("ServerSocket created, blocking on accept and waiting for incoming requests!");
+			Socket connectionSocket = welcomeSocket.accept();
+			
+			System.out.println("ServerSocket accepted incoming request from: " + connectionSocket.getPort());
+			
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+			System.out.println("Accepted TCP connection from" 
+					+ connectionSocket.getInetAddress() 
+					+ ":" + connectionSocket.getPort());
+			try {
+				while (true) {
+					clientSentence = inFromClient.readLine();
+
+					capitalizedSentence = clientSentence.toUpperCase() + '\n';
+
+					outToClient.writeBytes(capitalizedSentence);
+				}
+			} catch (Exception e) {
+				// TODO: handle exception, if client closed connection, print:
+				System.out.println("Client closed connection.");
+			}
+		}
+	}
+}
---
+++ nix/devShell.nix
@@ -0,0 +1,21 @@
+{
+  mkShell,
+  alejandra,
+  bash,
+  jdk23,
+}:
+mkShell rec {
+  name = "network-infra-lab-06";
+
+  packages = [
+    # Our Makefile requires a modern bash. If the developer computer is
+    # running macOS then it ships with an old broken version of bash.
+    # This ensures that the Makefile works. Alternately, we can just
+    # fix the Makefile.
+    bash
+    jdk23
+
+    # Required for CI for format checking.
+    alejandra
+  ];
+}
